d6fe95dfd25f204209cfba59d436058295163e8f
sunnywealthygen <163027777+sunnywealthygen@users.noreply.github.com>
2024-03-16 20:41:48 -0700

    Update powerchainer.py

--- powerchainer.py
+++ powerchainer.py
@@ -4,16 +4,23 @@ count=0
 k=''
 for line in Lines:
   k=line
+file1.close()
 print(k)
 k=str(int(k)+1)
 file2 = open('thepower.txt', 'w')
 file2.write(k)
-print('version 3')
+print('version 4')
 print(k)
+file2.close()
 
 with open('thepower.txt', 'w') as file_obj:
     file_obj.write("I love programming!\n")
     file_obj.write("I love Python!\n")
-
+file_obj.close()
 with open('thepower.txt', 'r') as file_obj:
     print(file_obj.readlines())
+file_obj.close()
+with open('thepower.txt', 'r') as file_obj:
+    print(file_obj.readlines())
+file_obj.close()
+
