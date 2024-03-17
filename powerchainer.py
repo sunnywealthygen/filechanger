1b04afcfbaac22460db3e5cc3cfa414251556c88
sunnywealthygen <163027777+sunnywealthygen@users.noreply.github.com>
2024-03-16 20:21:34 -0700

    Create powerchainer.py

---
+++ powerchainer.py
@@ -0,0 +1,10 @@
+file1 = open('thepower.txt', 'r')
+Lines = file1.readlines()
+count=0
+k=''
+for line in Lines:
+  k=line
+
+file1 = open('thepower.txt', 'w')
+file1.writelines(str(int(k)+1))
+file1.close()
